45f0a2f7d01358049079cc02d240f37192b9f799
Ardika Rommy Sanjaya <contact@ardikars.com>
2020-03-16 19:37:48 +0700

    Fix validate token

--- src/main/java/com/cmsnesia/storage/config/security/SecurityContextRepository.java
+++ src/main/java/com/cmsnesia/storage/config/security/SecurityContextRepository.java
@@ -1,13 +1,12 @@
 package com.cmsnesia.storage.config.security;
 
 import com.cmsnesia.model.AuthDto;
-import com.cmsnesia.model.response.TokenResponse;
 import com.cmsnesia.sdk.client.TokenService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -15,10 +14,12 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.web.server.context.ServerSecurityContextRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.util.Set;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -26,7 +27,7 @@ import java.util.stream.Collectors;
 @Component
 public class SecurityContextRepository implements ServerSecurityContextRepository {
 
-  private final TokenService tokenService;
+  private final ObjectMapper objectMapper;
 
   @Override
   public Mono<Void> save(ServerWebExchange exchange, SecurityContext context) {
@@ -36,22 +37,55 @@ public class SecurityContextRepository implements ServerSecurityContextRepositor
   @Override
   public Mono<SecurityContext> load(ServerWebExchange exchange) {
     ServerHttpRequest request = exchange.getRequest();
-    String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-    if (authHeader != null && authHeader.startsWith("Bearer ")) {
-      String authToken = authHeader.substring(7);
-      Mono<AuthDto> authDtoMono = tokenService.validate(new TokenResponse(authToken, "", "Bearer"));
-      return authDtoMono.map(
-          authDto -> {
-            Set<GrantedAuthority> authorities =
-                authDto.getRoles().stream()
-                    .map(role -> new SimpleGrantedAuthority(role))
-                    .collect(Collectors.toSet());
-            Authentication authentication =
-                new UsernamePasswordAuthenticationToken(authDto, null, authorities);
-            return new SecurityContextImpl(authentication);
-          });
-    } else {
-      return Mono.empty();
+    String userDataJson = request.getHeaders().getFirst("X-User-Data");
+    if (StringUtils.hasText(userDataJson)) {
+      try {
+        AuthDto authDto = objectMapper.readValue(userDataJson, AuthDto.class);
+        SecurityContext securityContext =
+            new SecurityContextImpl(
+                new Authentication() {
+                  @Override
+                  public Collection<? extends GrantedAuthority> getAuthorities() {
+                    return authDto.getRoles() == null
+                        ? new HashSet<>()
+                        : authDto.getRoles().stream()
+                            .map(role -> new SimpleGrantedAuthority(role))
+                            .collect(Collectors.toSet());
+                  }
+
+                  @Override
+                  public Object getCredentials() {
+                    return authDto.getPassword();
+                  }
+
+                  @Override
+                  public Object getDetails() {
+                    return authDto;
+                  }
+
+                  @Override
+                  public Object getPrincipal() {
+                    return authDto;
+                  }
+
+                  @Override
+                  public boolean isAuthenticated() {
+                    return true;
+                  }
+
+                  @Override
+                  public void setAuthenticated(boolean b) throws IllegalArgumentException {}
+
+                  @Override
+                  public String getName() {
+                    return authDto.getFullName();
+                  }
+                });
+        return Mono.just(securityContext);
+      } catch (JsonProcessingException e) {
+        return Mono.empty();
+      }
     }
+    return Mono.empty();
   }
 }
